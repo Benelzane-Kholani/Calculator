2feffbff58355789822f23356a73aed0ece4faa5
danie <daniellaburgessbqueen@outlook.com>
2021-04-01 16:57:12 +0200

    Added //219446482

--- src/main/java/za/ac/cput/calculator/Calculator.java
+++ src/main/java/za/ac/cput/calculator/Calculator.java
@@ -7,8 +7,13 @@ package za.ac.cput.calculator;
 
 /**
  *
- * @author Johnson
+ * @author 219446482
  */
 public class Calculator {
+    
+    public int division(int a, int b){
+        if (a<1)return 0;
+        return a/b;
+    }
     
 }
