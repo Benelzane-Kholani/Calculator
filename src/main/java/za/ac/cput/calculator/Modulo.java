4db10b19c098e72818cc7a62521675f98430fcc6
ColeColombia <benzemabenelzane@yahoo.com>
2021-04-01 10:31:32 +0200

    Im done with the modulo class and ModuloTest class

--- src/main/java/za/ac/cput/calculator/Modulo.java
+++ src/main/java/za/ac/cput/calculator/Modulo.java
@@ -1,9 +1,13 @@
-<<<<<<< HEAD
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+/*
+*This class is used to calculate the modulus of two numbers
+*/
 package za.ac.cput.calculator;
 
 /**
@@ -11,47 +15,51 @@ package za.ac.cput.calculator;
  * @author Benelzane Kholani 218257465
  */
 public class Modulo {
-    
+
     private int number1;
     private int number2;
-    
+
+    public Modulo()
+    {
+    }//end of null agurmanet constructor
+
     public Modulo(int number1, int number2)
     {
         this.number1 = number1;
         this.number2 = number2;
-    }
-    
+    }//end of modulo constructor
+
     public void setNumber1(int number1)
     {
         this.number1 = number1;
-    }
-    
+    }//end of setNumber1 method
+
     public int getNumber1()
     {
         return this.number1;
-    }
-    
+    }//end of getNumber1 method
+
     public void setNumber2(int number2)
     {
         this.number1 = number2;
-    }
-    
+    }//end of setNumber2 method
+
     public int getNumber2()
     {
         return this.number2;
-    }
-    
-    public int Answer()
-    { 
-    return this.number1 % this.number2;  
-    }
-    
-    public String toString()
-    {
-        return String.format("The modulo is %d",this.Answer());
-    }
-    
-}
-=======
+    }//end of getNumber2 method
 
->>>>>>> 51f2bc2fbea2e4783bf37186707b636e9a51ae24
+    public int answer()
+    {
+    int modulo;
+
+    modulo = this.number1 % this.number2;
+    return modulo;
+    }//end of answer method
+
+
+    public String toString(){
+    return String.format("The modulus of %d and %d is %d",this.number1,this.number2,answer());
+}//end of toString method
+
+}//end of Modulo class
